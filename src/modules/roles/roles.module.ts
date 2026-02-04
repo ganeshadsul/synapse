7a9e3d4d65f855d72cc4a027c80bfbdc4e585cd2
Ganesh Adsul <ganeshadsul76@gmail.com>
2026-02-04 21:37:59 +0530

    minor changes

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -2,11 +2,11 @@ import { Logger, Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
-import { RolesController } from './v1/roles.controller';
-//
+import { RolesController as V1RolesController } from './v1/roles.controller';
+
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
   providers: [RolesService, Logger],
-  controllers: [RolesController],
+  controllers: [V1RolesController],
 })
 export class RolesModule {}
