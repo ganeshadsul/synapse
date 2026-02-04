2382a0cfc671d7beff69d5adf3f15f7ff20cfb9c
Ganesh Adsul <ganeshadsul76@gmail.com>
2026-02-04 23:13:27 +0530

    Role module updated and seeder added

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -2,11 +2,11 @@ import { Logger, Module } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
-import { RolesController as V1RolesController } from './v1/roles.controller';
+import { RoleController as V1RoleController } from './v1/role.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
   providers: [RoleService, Logger],
-  controllers: [V1RolesController],
+  controllers: [V1RoleController],
 })
 export class RoleModule {}
